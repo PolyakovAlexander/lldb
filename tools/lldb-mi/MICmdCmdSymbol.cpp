cc96e54e4c69e7308ed8d130a340db1b7f5a2496
Alexander Polyakov <polyakov.alx@gmail.com>
2018-07-04 01:40:15 +0300

    ammend kast commit

--- tools/lldb-mi/MICmdCmdSymbol.cpp
+++ tools/lldb-mi/MICmdCmdSymbol.cpp
@@ -39,6 +39,8 @@ CreateMITuplePCLine(const uint32_t addr, const uint32_t line_number) {
 }
 } // namespace
 
+using namespace lldb;
+
 //++
 //------------------------------------------------------------------------------------
 // Details: CMICmdCmdSymbolListLines constructor.
